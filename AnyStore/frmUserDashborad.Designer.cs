ea44680e04312904ea3ef021446994edfbb69216
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-06-24 16:19:33 +0530

    Finish Admin Dashboard User Interface

--- AnyStore/frmUserDashborad.Designer.cs
+++ AnyStore/frmUserDashborad.Designer.cs
@@ -28,12 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblSHead = new System.Windows.Forms.Label();
+            this.lblAppLName = new System.Windows.Forms.Label();
+            this.lblAppFName = new System.Windows.Forms.Label();
+            this.lblLoggedInUser = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFooter = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblSHead);
+            this.panel2.Controls.Add(this.lblAppLName);
+            this.panel2.Controls.Add(this.lblAppFName);
+            this.panel2.Controls.Add(this.lblLoggedInUser);
+            this.panel2.Controls.Add(this.lblUser);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1350, 729);
+            this.panel2.TabIndex = 2;
+            // 
+            // lblSHead
+            // 
+            this.lblSHead.AutoSize = true;
+            this.lblSHead.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSHead.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.lblSHead.Location = new System.Drawing.Point(532, 367);
+            this.lblSHead.Name = "lblSHead";
+            this.lblSHead.Size = new System.Drawing.Size(314, 25);
+            this.lblSHead.TabIndex = 7;
+            this.lblSHead.Text = "Billing And Inventory Management";
+            // 
+            // lblAppLName
+            // 
+            this.lblAppLName.AutoSize = true;
+            this.lblAppLName.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppLName.Location = new System.Drawing.Point(678, 330);
+            this.lblAppLName.Name = "lblAppLName";
+            this.lblAppLName.Size = new System.Drawing.Size(85, 37);
+            this.lblAppLName.TabIndex = 6;
+            this.lblAppLName.Text = "Store";
+            // 
+            // lblAppFName
+            // 
+            this.lblAppFName.AutoSize = true;
+            this.lblAppFName.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppFName.Location = new System.Drawing.Point(613, 330);
+            this.lblAppFName.Name = "lblAppFName";
+            this.lblAppFName.Size = new System.Drawing.Size(69, 37);
+            this.lblAppFName.TabIndex = 5;
+            this.lblAppFName.Text = "ANY";
+            // 
+            // lblLoggedInUser
+            // 
+            this.lblLoggedInUser.AutoSize = true;
+            this.lblLoggedInUser.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoggedInUser.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lblLoggedInUser.Location = new System.Drawing.Point(49, 43);
+            this.lblLoggedInUser.Name = "lblLoggedInUser";
+            this.lblLoggedInUser.Size = new System.Drawing.Size(137, 17);
+            this.lblLoggedInUser.TabIndex = 4;
+            this.lblLoggedInUser.Text = "Pasith Senevirathana";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(13, 43);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(38, 17);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "User:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.lblFooter);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 687);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1350, 42);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.AutoSize = true;
+            this.lblFooter.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFooter.ForeColor = System.Drawing.Color.White;
+            this.lblFooter.Location = new System.Drawing.Point(572, 13);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(207, 17);
+            this.lblFooter.TabIndex = 0;
+            this.lblFooter.Text = "Developed By: Pasith Senevirathna";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userToolStripMenuItem,
+            this.categoryToolStripMenuItem,
+            this.inventoryToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1350, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStripTop";
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.userToolStripMenuItem.Text = "Purchase";
+            // 
+            // categoryToolStripMenuItem
+            // 
+            this.categoryToolStripMenuItem.Name = "categoryToolStripMenuItem";
+            this.categoryToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.categoryToolStripMenuItem.Text = "Sales ";
+            // 
+            // inventoryToolStripMenuItem
+            // 
+            this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
+            this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.inventoryToolStripMenuItem.Text = "Inventory";
+            // 
+            // frmUserDashborad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmUserDashborad";
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.panel2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1366, 768);
+            this.MinimumSize = new System.Drawing.Size(1366, 768);
+            this.Name = "frmUserDashborad";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User Dashborad";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblSHead;
+        private System.Windows.Forms.Label lblAppLName;
+        private System.Windows.Forms.Label lblAppFName;
+        private System.Windows.Forms.Label lblLoggedInUser;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblFooter;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventoryToolStripMenuItem;
     }
 }
