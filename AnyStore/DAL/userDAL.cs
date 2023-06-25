3eafff953c6370b96a84f386514ee15600bd51eb
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-06-25 19:49:13 +0530

    Create new Class, Add Connection String(line 12-14)

---
+++ AnyStore/DAL/userDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.DAL
+{
+    internal class userDAL
+    {
+        #region Connection String 
+        static string myConString = ConfigurationManager.ConnectionStrings["AnyStor"].ConnectionString;
+        #endregion
+    }
+}
