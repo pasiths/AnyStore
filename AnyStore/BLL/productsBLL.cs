6eb0aa83079f87564c1e1b3087fdf32ccb796914
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-06-26 12:10:05 +0530

    create class productsBLL and use encapsulation

---
+++ AnyStore/BLL/productsBLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    internal class productsBLL
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int category { get; set; }
+        public string description { get; set; }
+        public double rate { get; set; }
+        public double qty { get; set; }
+        public DateTime added_date { get; set; }
+        public int added_by { get; set; }
+    }
+}
