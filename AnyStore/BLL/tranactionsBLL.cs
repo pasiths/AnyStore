0d9bd2992dd764c873884d44a226a94e8fbcb5e8
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-06-26 12:14:27 +0530

    create class tranactionsBLL and use encapsulation

---
+++ AnyStore/BLL/tranactionsBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.BLL
+{
+    internal class tranactionsBLL
+    {
+        public int id { get; set; }
+        public int dea_cust_id { get; set; }
+        public double grandTotal{get; set; }
+        public DateTime transaction_date { get; set; }
+        public double tax { get; set; }
+        public double discount { get; set; }
+        public int added_by { get; set; }
+    }
+}
