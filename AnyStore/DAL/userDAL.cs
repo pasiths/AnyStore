a77831c9080eb868410b3be7ecbc9c5a3a1cf40f
Pasith <pasiths@users.noreply.github.com>
2023-06-28 21:36:19 +0530

    Create Select Method

--- AnyStore/DAL/userDAL.cs
+++ AnyStore/DAL/userDAL.cs
@@ -1,16 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AnyStore.DAL
 {
     internal class userDAL
     {
         #region Connection String 
-        static string myConString = ConfigurationManager.ConnectionStrings["AnyStor"].ConnectionString;
+        static string myConnection = ConfigurationManager.ConnectionStrings["AnyStore"].ConnectionString;
+        #endregion
+
+        #region SelectData From Database
+        public DataTable Select()
+        {
+            //Static Method To Connect Database
+            SqlConnection con = new SqlConnection(myConnection);
+            //To Hold The Data From Database
+            DataTable dt = new DataTable();
+            try
+            {
+                //SQL Query To Get Data From Database
+                String sql = "SELECT * FROM tbl_users";
+                //For Executing Command
+                SqlCommand cmd = new SqlCommand(sql, con);
+                //Getting Data From Database
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                //Database Connection Open
+                con.Open();
+                //Fill Data In Our Datatable
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                //Throw Message if any error occurs
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Closing Connection
+                con.Close();
+            }
+            //Return the value in DataTable
+            return dt;
+        }
         #endregion
     }
 }
