14add4b71e134f79774efaf521bb449caf57294b
Pasith-Home <pasiya20024@gmail.com>
2023-07-01 20:56:08 +0530

    Insert Data in Database

--- AnyStore/DAL/userDAL.cs
+++ AnyStore/DAL/userDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnyStore.BLL;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -48,6 +49,57 @@ namespace AnyStore.DAL
             }
             //Return the value in DataTable
             return dt;
+        }
+        #endregion
+
+        #region Insert Data in Database
+        public bool Insert(userBLL u)
+        {
+            bool isSuccess=false;
+            SqlConnection con=new SqlConnection(myConnection);
+
+            try
+            {
+                String sql = "INSERT INTO tbl_users(first_name,last_name,email,username,password,address,gender,user_type,added_date,added_by) VALUES(@first_name,@last_name,@email,@username,@password,@address,@gender,@user_type,@added_date,@added_by)";
+                SqlCommand cmd=new SqlCommand(sql, con);
+
+                cmd.Parameters.AddWithValue("@first_name", u.first_name);
+                cmd.Parameters.AddWithValue("@last_name", u.last_name);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@username", u.username);
+                cmd.Parameters.AddWithValue("@password", u.first_name);
+                cmd.Parameters.AddWithValue("@address", u.address);
+                cmd.Parameters.AddWithValue("@gender", u.gender);
+                cmd.Parameters.AddWithValue("@user_type", u.user_type);
+                cmd.Parameters.AddWithValue("@added_date", u.added_date);
+                cmd.Parameters.AddWithValue("@added_by", u.added_by);
+
+                con.Open();
+
+                int rows=cmd.ExecuteNonQuery();
+
+                //If the query is executed Successfully then the value to rows will be greater than 0 else it will be less than 0
+                if(rows > 0)
+                {
+                    //Query Successfull
+                    isSuccess=true;
+                }
+                else
+                {
+                    //Query Failed
+                    isSuccess=false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return isSuccess;
         }
         #endregion
     }
