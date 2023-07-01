019d2a3133878d40e8b847f01ab37aa3e5436c3f
Pasith-Home <pasiya20024@gmail.com>
2023-07-01 21:09:01 +0530

    Update data in Database

--- AnyStore/DAL/userDAL.cs
+++ AnyStore/DAL/userDAL.cs
@@ -102,5 +102,55 @@ namespace AnyStore.DAL
             return isSuccess;
         }
         #endregion
+
+        #region Update data in Database
+        public bool Update(userBLL u)
+        {
+            bool isSuccess = false;
+
+            SqlConnection con = new SqlConnection(myConnection);
+
+            try
+            {
+                string sql = "UPDATE tbl_users SET first_name=@first_name,last_name=@last_name,email=@email,username=@username,password=@password,address=@address,gender=@gender,user_type=@user_type,added_date=@added_date,added_by=@added_by WHERE user_id=@id";
+                SqlCommand cmd=new SqlCommand(sql, con);
+
+                cmd.Parameters.AddWithValue("@first_name", u.first_name);
+                cmd.Parameters.AddWithValue("@last_name", u.last_name);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@username", u.username);
+                cmd.Parameters.AddWithValue("@password", u.first_name);
+                cmd.Parameters.AddWithValue("@address", u.address);
+                cmd.Parameters.AddWithValue("@gender", u.gender);
+                cmd.Parameters.AddWithValue("@user_type", u.user_type);
+                cmd.Parameters.AddWithValue("@added_date", u.added_date);
+                cmd.Parameters.AddWithValue("@added_by", u.added_by);
+                cmd.Parameters.AddWithValue("@id", u.id);
+
+                con.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            
+            return isSuccess;
+        }
+        #endregion
     }
 }
