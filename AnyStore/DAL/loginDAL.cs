2fb998643be01c70b6a71c24ac6990695b2a3d5a
Pasith-Home <pasiya20024@gmail.com>
2023-07-03 18:56:05 +0530

    create new class,

---
+++ AnyStore/DAL/loginDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyStore.DAL
+{
+    internal class loginDAL
+    {
+        #region Connection String 
+        //Static String to Connect Database
+        static string myconnectionstring = ConfigurationManager.ConnectionStrings["AnyStore"].ConnectionString;
+        #endregion
+    }
+}
